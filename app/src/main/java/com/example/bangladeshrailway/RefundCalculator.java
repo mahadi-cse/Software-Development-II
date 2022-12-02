49a9422c55ed1fe8ea2ab3c29886a1d62767a0aa
mahadinina <mh952219@gmail.com>
2022-12-02 18:41:35 +0600

    Main Interface Image resolution fit with screen .

--- app/src/main/java/com/example/bangladeshrailway/RefundCalculator.java
+++ app/src/main/java/com/example/bangladeshrailway/RefundCalculator.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.Objects;
 
@@ -17,6 +21,47 @@ public class RefundCalculator extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setTitle("Refund Calculator");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        Spinner spinner = findViewById(R.id.spinner_class);
+        Spinner personSelect = findViewById(R.id.person_spinner);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.class_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String textClass = parent.getItemAtPosition(position).toString();
+//                Toast.makeText(BuyTickets.this, textClass, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+        ArrayAdapter<CharSequence> adapterPerson = ArrayAdapter.createFromResource(this,
+                R.array.person_count, android.R.layout.simple_spinner_item);
+        adapterPerson.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        personSelect.setAdapter(adapterPerson);
+        personSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String textPerson = parent.getItemAtPosition(position).toString();
+//                Toast.makeText(BuyTickets.this, textPerson, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/bangladeshrailway/UserActivity.java
+++ app/src/main/java/com/example/bangladeshrailway/UserActivity.java
@@ -18,7 +18,7 @@ public class UserActivity extends Fragment implements View.OnClickListener{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.activity_user_profile, container, false);
+        View view = inflater.inflate(R.layout.activity_user_activity, container, false);
 
         buyTickets=view.findViewById(R.id.buy_tickets);
         fare=view.findViewById(R.id.fare);
